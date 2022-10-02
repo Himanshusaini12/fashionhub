e22252191adcd1940b1a0ac5ba418bedcacac217
himanshusaini12 <himanshusaini962001@gmail.com>
2022-10-02 08:22:43 +0000

    Update 4 files

--- src/components/collection-item/collection-item.js
+++ src/components/collection-item/collection-item.js
@@ -1,18 +1,20 @@
 import React from "react";
+import CustomButton from "../custome-button/custome-button";
+import "../collection-item/collection-item.scss";
 
-import '../collection-item/collection-item.scss'
-
-const CollectionItem =({id,name,price,imageUrl})=>(
-    <div className="collection-item">
-       
-        <div className='image'  style={{
-    backgroundImage:`url(${imageUrl})` 
-  }}/>
-  < div className="collection-footer" >
-    <div className="name">{name}</div>
-    <div className="price">{price}</div>
-  </div>
-      a
+const CollectionItem = ({ id, name, price, imageUrl }) => (
+  <div className="collection-item">
+    <div
+      className="image"
+      style={{
+        backgroundImage: `url(${imageUrl})`,
+      }}
+    />
+    <div className="collection-footer">
+      <div className="name">{name}</div>
+      <div className="price">{price}</div>
     </div>
-)
-export default CollectionItem
+    <CustomButton>ADD TO CART</CustomButton>
+  </div>
+);
+export default CollectionItem;
