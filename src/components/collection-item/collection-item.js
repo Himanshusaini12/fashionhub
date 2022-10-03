3c2dab47e3a864794463d2fd0d9083147d796895
himanshusaini12 <himanshusaini962001@gmail.com>
2022-10-03 07:01:51 +0000

    add to cart added

--- src/components/collection-item/collection-item.js
+++ src/components/collection-item/collection-item.js
@@ -1,20 +1,28 @@
 import React from "react";
 import CustomButton from "../custome-button/custome-button";
 import "../collection-item/collection-item.scss";
-
-const CollectionItem = ({ id, name, price, imageUrl }) => (
-  <div className="collection-item">
-    <div
-      className="image"
-      style={{
-        backgroundImage: `url(${imageUrl})`,
-      }}
-    />
-    <div className="collection-footer">
-      <div className="name">{name}</div>
-      <div className="price">{price}</div>
+import { connect } from "react-redux";
+import addItem from "../../redux/cart/cart.action";
+const CollectionItem = ({ item, addItem }) => {
+  const { name, price, imageUrl } = item;
+  return (
+    <div className="collection-item">
+      <div
+        className="image"
+        style={{
+          backgroundImage: `url(${imageUrl})`,
+        }}
+      />
+      <div className="collection-footer">
+        <div className="name">{name}</div>
+        <div className="price">{price}</div>
+      </div>
+      <CustomButton onClick={addItem}>ADD TO CART</CustomButton>
     </div>
-    <CustomButton>ADD TO CART</CustomButton>
-  </div>
-);
-export default CollectionItem;
+  );
+};
+
+const mapDispatchToProps = (dispatch) => ({
+  addItem: (item) => dispatch(addItem(item)),
+});
+export default connect(null, mapDispatchToProps)(CollectionItem);
--- src/components/preview-comp/preview.js
+++ src/components/preview-comp/preview.js
@@ -1,16 +1,20 @@
 import React from "react";
-import '../preview-comp/preview.scss'
+import "../preview-comp/preview.scss";
 import CollectionItem from "../collection-item/collection-item";
 
-const CollectionPreview=({title,items})=>(
-
+const CollectionPreview = ({ title, items }) => {
+  return (
     <div className="collection-preview">
-        <h1 className="title">{title.toUpperCase()}</h1>
-        <div className="preview" >
-           {
-            items.filter((items,idx)=>idx<4).map(({id,...otherItemprops})=>(<CollectionItem key={id} {...otherItemprops} />))
-           }   
+      <h1 className="title">{title.toUpperCase()}</h1>
+      <div className="preview">
+        {items
+          .filter((items, idx) => idx < 4)
+          .map((item) => (
+            <CollectionItem key={item.id} item={item} />
+          ))}
+      </div>
     </div>
-    </div>
-);
-export default CollectionPreview 
+  );
+};
+
+export default CollectionPreview;
