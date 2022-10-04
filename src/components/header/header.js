126c6a519b26c850fe49a8dca7b7b97f861bf6bd
Himanshusaini12 <107533833+Himanshusaini12@users.noreply.github.com>
2022-10-04 23:10:32 +0530

    Update header.js

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -29,7 +29,7 @@ const Header = ({ currentUser, hidden }) => (
       </Link>
       {currentUser ? (
         <button className="option" onClick={() => auth.signOut()}>
-          Welcome{currentUser.displayName}
+          Hi{currentUser.displayName}
         </button>
       ) : (
         <Link className="option" to="/signin">
