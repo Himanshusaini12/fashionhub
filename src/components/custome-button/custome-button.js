3edbd936d24c007c5cbc73b90ddfee43552488f1
himanshusaini12 <himanshusaini962001@gmail.com>
2022-11-06 06:11:11 +0000

    cartdropdown updated

--- src/components/custome-button/custome-button.js
+++ src/components/custome-button/custome-button.js
@@ -1,13 +1,11 @@
 import React from "react";
-import "../../components/custome-button/custome-button.scss";
 
-const CustomButton = ({ children, googleSignin, inverted,...otherProps }) => (
-  <button
-    className={`${inverted ? "googleSignin" : " "}${googleSignin ? "googleSignin" : " "} custom-button `}
-    {...otherProps}
-  >
+import Button from "@mui/material/Button";
+
+const CustomButton = ({ children, googleSignin, inverted, ...otherProps }) => (
+  <Button variant="contained" {...otherProps}>
     {children}
-  </button>
+  </Button>
 );
 
 export default CustomButton;
